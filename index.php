488cd878a37c4e72d86588a0a4c9b90e618f1150
unknown <tiarrotamba24@gmail.com>
2020-02-22 17:04:38 +0700

    Change file extension to php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    echo "Hello Word";
+?>
+<html>
+    <head>
+        <title>Biodata</title>
+    </head>
+    <body>
+        <h1>Biodata Diriku</h1>
+        <h2>Jangan di ganti yah bidata ku</h2>
+    </body>
+</html>
