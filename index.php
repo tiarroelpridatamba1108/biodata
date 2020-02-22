3b78e86a44e8fee9b4fad9abe7b842262d690e4a
unknown <tiarrotamba24@gmail.com>
2020-02-22 17:24:54 +0700

    perubahan

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     </head>
     <body>
         <h1>Biodata Diriku</h1>
-        <h2>Jangan di ganti yah bidata ku</h2>
+        <h2>Jangan di ganti yah biodata ku</h2>
         <h3>ganti</h3>
     </body>
 </html>
